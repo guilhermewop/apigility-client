248eb6d7743177fae3fb23cbbf845deef61bc860
Guilherme Pereira <guilhermewop@gmail.com>
2015-03-12 18:03:48 -0300

    simpler exception message

--- src/ApigilityClient/Http/Response/TriggerException.php
+++ src/ApigilityClient/Http/Response/TriggerException.php
@@ -12,29 +12,20 @@ class TriggerException
     /**
      * Throws an exception
      *
-     * @param  Zend\Http\Client                                   $client
-     * @param  Zend\Http\Response                                 $response
-     * @param  string|null                                        $message
+     * @param  Zend\Http\Client   $client
+     * @param  Zend\Http\Response $response
+     * @param  string|null        $message
      * @throws ApigilityClient\Exception\RuntimeException
      */
     public function __construct(ZendHttpClient $client, ZendHttpResponse $response, $message = null)
     {
-        $defaultMessage = 'The API response is not an expected response';
-        $message        = (string) $message;
-        $message        = (empty($message)) ? $defaultMessage : $defaultMessage . PHP_EOL . $message;
+        $error = json_decode($response->getBody());
 
         throw new RuntimeException(sprintf(
-            $message . ': ' . PHP_EOL .
-            'Request  -> Uri: %s ' . PHP_EOL .
-            'Request  -> GET params: %s ' . PHP_EOL .
-            'Request  -> POST params: %s ' . PHP_EOL .
-            'Response -> HTTP code: %s ' . PHP_EOL .
-            'Response -> Body: %s',
-            $client->getMethod() . ' ' . $client->getUri()->toString(),
-            $client->getRequest()->getQuery()->toString(),
-            $client->getRequest()->getPost()->toString(),
-            $response->getStatusCode(),
-            $response->getBody()
+            'Erro "%s/%s". %s',
+            $error->status,
+            $error->title,
+            $error->detail
         ));
     }
 }
